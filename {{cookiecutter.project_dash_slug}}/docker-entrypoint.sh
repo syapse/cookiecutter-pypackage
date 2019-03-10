68e0b63d49e488cb4c3c89277b51da3b645990c1
Gianluca <gianlucab@syapse.com>
2019-03-10 01:47:04 -0800

    Refactoring

--- {{cookiecutter.project_dash_slug}}/docker-entrypoint.sh
+++ {{cookiecutter.project_dash_slug}}/docker-entrypoint.sh
@@ -12,9 +12,9 @@ run_cli() {
   export NEW_RELIC_CONFIG_FILE
   if [ -n "${NEW_RELIC_LICENSE_KEY}" ]; then
     echo "Running with New Relic monitoring..."
-    exec newrelic-admin run-program {{cookiecutter.project_dash_slug}} "$@"
+    exec newrelic-admin run-program python {{cookiecutter.project_dash_slug}}/cli.py "$@"
   else
-    exec {{cookiecutter.project_dash_slug}} "$@"
+    exec python {{cookiecutter.project_dash_slug}}/cli.py "$@"
   fi
 }
 
---
+++ {{cookiecutter.project_dash_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for {{cookiecutter.project_slug}}."""
+import sys
+import click
+import {{cookiecutter.project_slug}}
+
+
+@click.command()
+def main():
+    """Console script for {{cookiecutter.project_slug}}."""
+    click.echo("Replace this message by putting your code into "
+               "{{cookiecutter.project_slug}}.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
