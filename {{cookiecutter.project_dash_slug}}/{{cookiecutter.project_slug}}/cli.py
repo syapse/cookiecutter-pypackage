a798df5e919921f6374b53792502a108e85825a3
Gianluca <gianlucab@syapse.com>
2019-03-11 13:49:51 -0700

    add config template

--- {{cookiecutter.project_dash_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_dash_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -3,15 +3,17 @@
 """Console script for {{cookiecutter.project_slug}}."""
 import sys
 import click
+from decouple import config
+
+USER = config("USER")
 
 
 @click.command()
 def main():
     """Console script for {{cookiecutter.project_slug}}."""
-    click.echo("Replace this message by putting your code into "
-               "{{cookiecutter.project_slug}}.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
+    click.echo(
+        f"Hello, {USER}! Replace this message by putting your code into "
+        f"{{cookiecutter.project_slug}}.cli.main")
 
 
 if __name__ == "__main__":
